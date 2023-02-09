f8b3c55c57d75836511df05dc87fbb18b89f86aa
Ayushman <maanayush1002@gmail.com>
2023-02-09 09:58:58 +0530

    continue example
    c++ program to show the working of continue

---
+++ continueExample.cpp
@@ -0,0 +1,17 @@
+//c++ program to show the working of continue
+#include<iostream>
+using namespace std;
+
+int main(){
+    for (int i = 0; i <=5; i++)
+    {
+        /* code */
+        cout<<"Hi"<<endl;;
+        cout<<"hello"<<endl;;
+        continue;
+        cout<<"Reply toh karde"<<endl;
+    }
+    
+
+return 0;
+}
